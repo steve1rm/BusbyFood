8168cae04d3139e03c06b95dc95010b2d10d6840
Steve <steve@kitkat@gmail.com>
2022-07-22 17:15:22 +0700

    Add icons for apple, facebook, and the 1 connect

--- presentation/src/main/java/me/androidbox/busbyfood/screen/LoginScreen.kt
+++ presentation/src/main/java/me/androidbox/busbyfood/screen/LoginScreen.kt
@@ -1,7 +1,9 @@
 package me.androidbox.busbyfood.screen
 
+import android.widget.ImageButton
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,20 +35,41 @@ fun LoginScreen() {
 
         Spacer(modifier = Modifier.height(80.dp))
         Text(text = "Email *", fontSize = 16.sp, fontWeight = FontWeight.Bold)
-
         EmailEntry { emailText ->
             emailEntered = emailText
             println("Email Entered: $emailEntered")
         }
         Spacer(modifier = Modifier.height(40.dp))
-        Text(text = "Password *", fontSize = 16.sp, fontWeight = FontWeight.Bold)
-
+        Text(
+            text = "Password *",
+            fontSize = 16.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
         PasswordEntry()
         Spacer(modifier = Modifier.height(40.dp))
         ForgotPasswordLink()
         Spacer(modifier = Modifier.height(24.dp))
         LoginButton(text = "Login", textColor = Color.White, backgroundColor = Color.LightGray) {
             println("Email Entered: $emailEntered")
+        }
+        Spacer(modifier = Modifier.height(60.dp))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Or login with:",
+            color = Color.Red,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center)
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+
+            Button(onClick = { /*TODO*/ }) {
+
+            }
+            Button(onClick = { /*TODO*/ }) {
+
+            }
         }
     }
 }
